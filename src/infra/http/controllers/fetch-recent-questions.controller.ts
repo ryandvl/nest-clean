726b5787f37df387be6ad7030a6a9fcc882b7675
ryandvl <contatoryandvl55@gmail.com>
2025-02-04 18:28:22 -0300

    feat: fetch-recent-questions

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard';
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe';
-import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { z } from 'zod';
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
 
 const pageQueryParamsSchema = z
   .string()
@@ -18,20 +18,14 @@ type PageQueryParamsSchema = z.infer<typeof pageQueryParamsSchema>;
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class FetchRecentQuestionsController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) {}
 
   @Get()
-  async handle(
-    @Query('page', queryValidationPipe) page: PageQueryParamsSchema,
-  ) {
+  handle(@Query('page', queryValidationPipe) page: PageQueryParamsSchema) {
     const perPage = 20;
 
-    const questions = await this.prisma.question.findMany({
-      take: perPage,
-      skip: (page - 1) * perPage,
-      orderBy: {
-        createdAt: 'desc',
-      },
+    const questions = this.fetchRecentQuestions.execute({
+      page,
     });
 
     return { questions };
